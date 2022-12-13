4e655d4164fc05bb1cae42be3e2ba2df4110a129
admin <dariamafteuta@gmail.com>
2022-12-13 14:50:02 +0200

    text and textStyle

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,9 +10,73 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello!'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        toolbarHeight: 200,
+        title: RichText(
+          textAlign: TextAlign.left,
+          text: const TextSpan(
+            children: [
+              TextSpan(
+                text: 'C',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Color.fromRGBO(37, 40, 71, 1.0),
+                ),
+              ),
+              TextSpan(
+                text: 'писок\n',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Color.fromRGBO(59, 62, 89, 1.0),
+                ),
+              ),
+              TextSpan(
+                text: 'и',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Color.fromRGBO(37, 40, 71, 1.0),
+                ),
+              ),
+              TextSpan(
+                text: 'нтересных',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Color.fromRGBO(59, 62, 89, 1.0),
+                ),
+              ),
+              TextSpan(
+                text: ' м',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Color.fromRGBO(37, 40, 71, 1.0),
+                ),
+              ),
+              TextSpan(
+                text: 'ест',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Color.fromRGBO(59, 62, 89, 1.0),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
