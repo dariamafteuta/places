6b85ea18bbc6ede966e6663bd5b609f27fac2b8b
Daria <107044001+dariamafteuta@users.noreply.github.com>
2022-12-14 22:38:54 +0200

    Revert "text and textStyle"

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,21 +10,9 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        toolbarHeight: 200,
-        title: const Text(
-          'Список\nинтересных мест',
-          style: TextStyle(
-            fontFamily: 'Roboto',
-            fontWeight: FontWeight.w700,
-            fontSize: 40,
-            color: Color.fromRGBO(37, 40, 71, 1.0),
-          ),
-        ),
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello!'),
       ),
     );
   }
