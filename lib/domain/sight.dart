dda88993dcd50c2b49bb338bf87eb9904f52aede
admin <dariamafteuta@gmail.com>
2022-12-15 13:51:38 +0200

    mocks

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  final String name;
+  final double lat;
+  final double lot;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight({required this.name, required this.lat, required this.lot, required this.url, required this.details, required this.type});
+}
